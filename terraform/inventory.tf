93371c4d5f268568503b2a90eda2ba98f2f27020
SyrkashevAV <159136859+SyrkashevAV@users.noreply.github.com>
2024-03-28 16:57:44 +0300

    Update inventory.tf

---
+++ terraform/inventory.tf
@@ -0,0 +1,7 @@
+resource "local_file" "ansible_inventory" {
+  content = templatefile("inventory.tmpl", {
+    build_ip = yandex_compute_instance.build[0].network_interface[0].nat_ip_address,
+    prod_ip  = yandex_compute_instance.build[1].network_interface[0].nat_ip_address
+  })
+  filename = "./../ansible/inventory.yaml"
+}
